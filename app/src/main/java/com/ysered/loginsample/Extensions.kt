2cfb42a69809fa8b26e1778abc9b663633960bef
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-06-06 00:21:56 +0300

    Safely cast variable.

--- app/src/main/java/com/ysered/loginsample/Extensions.kt
+++ app/src/main/java/com/ysered/loginsample/Extensions.kt
@@ -6,10 +6,10 @@ import android.os.Parcelable
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 
-inline fun <reified T: Activity> Activity.startActivity(vararg params: Pair<String, Any>) {
+inline fun <reified T : Activity> Activity.startActivity(vararg params: Pair<String, Any>) {
     val intent = Intent(this, T::class.java)
     for ((key, value) in params) {
-        intent.putExtra(key, value as Parcelable)
+        intent.putExtra(key, value as? Parcelable)
     }
     this.startActivity(intent)
 }
@@ -19,9 +19,8 @@ fun Activity.replaceFragment(id: Int, fragment: Fragment, addToBackStack: Boolea
         val transaction = this.supportFragmentManager
                 ?.beginTransaction()
                 ?.replace(id, fragment, null)
-        if (addToBackStack) {
+        if (addToBackStack)
             transaction?.addToBackStack(null)
-        }
         transaction?.commit()
     }
 }
