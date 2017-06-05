1308fee27bf4e1709ea51ed2add3173f3a52eb28
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-06-06 00:17:34 +0300

    Helper activity extensions.

---
+++ app/src/main/java/com/ysered/loginsample/Extensions.kt
@@ -0,0 +1,27 @@
+package com.ysered.loginsample
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Parcelable
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+
+inline fun <reified T: Activity> Activity.startActivity(vararg params: Pair<String, Any>) {
+    val intent = Intent(this, T::class.java)
+    for ((key, value) in params) {
+        intent.putExtra(key, value as Parcelable)
+    }
+    this.startActivity(intent)
+}
+
+fun Activity.replaceFragment(id: Int, fragment: Fragment, addToBackStack: Boolean = false) {
+    if (this is AppCompatActivity) {
+        val transaction = this.supportFragmentManager
+                ?.beginTransaction()
+                ?.replace(id, fragment, null)
+        if (addToBackStack) {
+            transaction?.addToBackStack(null)
+        }
+        transaction?.commit()
+    }
+}
