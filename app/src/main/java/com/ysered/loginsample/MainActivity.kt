ebe8f2bb8982c57e0d445cca0bdb43a451722e8b
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-06-06 00:18:55 +0300

    Show login fragment.

--- app/src/main/java/com/ysered/loginsample/MainActivity.kt
+++ app/src/main/java/com/ysered/loginsample/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.ysered.loginsample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.facebook.AccessToken
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val isExpired = AccessToken.getCurrentAccessToken()?.isExpired ?: true
+        if (isExpired) {
+            println("Expired!!!")
+            replaceFragment(R.id.contentFrame, LoginFragment())
+        } else {
+            println("Logged in!!!")
+        }
     }
 }
